50cfca6fb0edfc51e288c8ec063dd4df42e0263b
AAA <aaa@aaa.com>
2017-12-17 18:59:41 -0600

    first commit

---
+++ run.bat
@@ -0,0 +1,7 @@
+@echo off
+title Terminal for Bus-Tracking-App
+cd C:\Users\Anthony\Desktop\Bus Tracker App\Bus-Tracker-App
+set Flask_app=app.py
+set flask_debug=1
+@echo on
+cmd /k
---
+++ static/client.js
@@ -0,0 +1,32 @@
+  var socket = io.connect('https://agile-sea-57808.herokuapp.com')
+//var socket = io.connect('https://localhost:5000')
+
+  socket.on('connect', function() {
+            socket.emit('hello', {data: 'I\'m connected!'});
+    });
+
+
+    var input = document.getElementById("new_message_text");
+    var button = document.getElementById("send_new_message");
+    var clearButton = document.getElementById("clear_message_list_button")
+	var message_list = document.getElementById("message_list");
+
+    button.addEventListener("click", function() {
+      var text = input.value;
+      socket.emit("new_message", text);
+    })
+
+
+    socket.on("new_message_received", function(new_message){
+      // add message to message_list
+      var li = document.createElement("li");
+      li.innerHTML = new_message;
+      message_list.append(li);
+
+    })
+
+	clearButton.addEventListener("click", function(){
+	message_list.innerHTML=""
+	
+})
+
